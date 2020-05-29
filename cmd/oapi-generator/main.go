99dc68b533eb65cb095a6987947ca6b465b60351
Koichi Shiraishi <zchee.io@gmail.com>
2020-05-29 18:11:47 +0900

    cmd/oapi-generator: add oapi-generator command

---
+++ cmd/oapi-generator/main.go
@@ -0,0 +1,51 @@
+// Copyright 2020 The go-openapi-tools Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Command oapi-generator generates the Go API client code from OpenAPI or Swagger schema.
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/zchee/go-openapi-tools/pkg/compiler/oapigen"
+)
+
+const (
+	exitCode = 1
+)
+
+var (
+	flagSchemaType  string
+	flagPackageName string
+	flagOut         string
+)
+
+func init() {
+	flag.StringVar(&flagSchemaType, "schema", oapigen.OpenAPISchema.String(), "Schema type.")
+	flag.StringVar(&flagPackageName, "package", "api", "generate package name.")
+	flag.StringVar(&flagOut, "out", "", `write schema to specific directory. (default "current directory")`)
+}
+
+func main() {
+	flag.Parse()
+	log.SetFlags(0)
+	log.SetPrefix("oapi-generator: ")
+
+	if flag.NArg() == 0 {
+		flag.Usage()
+		os.Exit(exitCode)
+	}
+	fname := flag.Arg(0)
+
+	api, err := oapigen.NewAPI(fname, flagPackageName, flagSchemaType)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := api.Gen(flagOut); err != nil {
+		log.Fatal(err)
+	}
+}
